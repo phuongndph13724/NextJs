c5b93c17cf218e677b81a25de8f75c9b05695bc6
Phuong <dacphuongotp@gmail.com>
2022-07-26 22:37:09 +0700

    Update : GetStaticProps

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
+	const router = useRouter();
+	console.log('About query :', router.query);
 	return <div>AboutPage</div>;
+}
+
+export async function getServerSideProps() {
+	return {
+		props: {}, // will be passed to the page component as props
+	};
 }
