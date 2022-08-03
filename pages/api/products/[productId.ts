afc00f461d2b7924ff93f4ee989565a59aef17f9
Phuong <93896015+nguyendacphuong@users.noreply.github.com>
2022-08-04 00:05:44 +0700

    update Proxy requests to API server

--- pages/api/products/[productId.ts
+++ pages/api/products/[productId.ts
@@ -6,5 +6,5 @@ type Data = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ name: 'Nguyễn Đắc Phương' });
+	res.status(200).json({ name: 'Get product detail' });
 }
